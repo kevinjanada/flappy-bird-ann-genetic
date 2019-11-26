f3494f5fd13bb93d27673678e6ede9a3ea4274a2
kevin janada <kevin.janada@gmail.com>
2019-11-26 20:37:08 +0700

    installed lodash, created shell function for think, and move at class Rocket, add property brain (where we store neural net) and currentDecision

--- src/objects/Rocket.ts
+++ src/objects/Rocket.ts
@@ -1,7 +1,9 @@
 import * as Phaser from 'phaser'
 import * as rocketImg from '../assets/rocket.png' // Importing image need to use *
+import * as _ from 'lodash'
 
 type coordinates = { x: number, y: number }
+type brainOutput = [number, number]
 class Rocket {
   static textureKey = 'ROCKET'
   static image = rocketImg
@@ -9,6 +11,19 @@ class Rocket {
   speed: number
   scene: Phaser.Scene
   id: number | string
+  /**
+   * @brain - {Neural Network}
+   * @inputs
+   * - rocket's y location (1 node)
+   * - distance of asteroids (number of asteroids * 2 nodes)
+   * @hidden
+   * - ??
+   * @output
+   * - go up / go down / stay still (2 nodes)
+   *   e.g [1,0] = go up, [0,1] = go down, [0,0] = stay still
+   */
+  brain: any // FIXME: this is the type of neural network
+  currentDecision: 'UP' | 'DOWN' | 'STAY'
   constructor(Scene: Phaser.Scene, position: { x: number, y: number }, speed: number, id?: string | number) {
     this.scene = Scene
     this.gameObj = Scene.physics.add.sprite(position.x, position.y, Rocket.textureKey)
@@ -18,12 +33,6 @@ class Rocket {
     this.gameObj.setCollideWorldBounds(true);
     this.speed = speed;
     if (id) this.id = id
-  }
-  moveUp() {
-    this.gameObj.setVelocityY(-1 * this.speed)
-  }
-  moveDown() {
-    this.gameObj.setVelocityY(this.speed)
   }
   calculateDistance(pos1: coordinates, pos2: coordinates): number {
     const horizontal = Math.abs(pos2.x - pos1.x)
@@ -35,8 +44,35 @@ class Rocket {
     const currPos = { x: this.gameObj.x, y: this.gameObj.y }
     asteroids.forEach(asteroid => {
       const asteroidPos = { x: asteroid.x, y: asteroid.y }
-      const distance = this.calculateDistance(currPos, asteroidPos)
     })
+  }
+  makeDecision (brainOutput: brainOutput) {
+    const up = [1,0]
+    const down = [0,1]
+    const stay = [0,0]
+    if (_.isEqual(brainOutput, up)) {
+      this.currentDecision = 'UP'
+    }
+    if (_.isEqual(brainOutput, down)) {
+      this.currentDecision = 'DOWN'
+    }
+    if (_.isEqual(brainOutput, stay)) {
+      this.currentDecision = 'STAY'
+    }
+  }
+  move () {
+    if (this.currentDecision == 'UP') { 
+      this.moveUp()
+    }
+    if (this.currentDecision == 'DOWN') {
+      this.moveDown()
+    }
+  }
+  moveUp () {
+    this.gameObj.setVelocityY(-1 * this.speed)
+  }
+  moveDown () {
+    this.gameObj.setVelocityY(this.speed)
   }
 }
 
